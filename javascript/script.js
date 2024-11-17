3028aae09b2152573ebcaed71cfa39d80818a804
SIRO <jjh060510@gmail.com>
2024-11-17 15:36:44 +0900

    .

---
+++ javascript/script.js
@@ -0,0 +1,18 @@
+jQuery(document).ready(function(){
+    $('.navi>li').mouseover(function(){
+        $(this).find('.submenu').stop().slideDown(500);
+    }).mouseout(function(){
+        $(this).find('.submenu').stop().slideUp(500);
+    });
+
+
+    setInterval(function(){
+        $('.slidelist').delay(1000);
+        $('.slidelist').animate({marginTop:-300});
+        $('.slidelist').delay(2000);
+        $('.slidelist').animate({marginTop:-600});
+        $('.slidelist').delay(2000);
+        $('.slidelist').animate({marginTop:0});
+        $('.slidelist').delay(2000);
+    });
+});
